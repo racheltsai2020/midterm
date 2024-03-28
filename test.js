b22123cc594f71932b462da06a25732677f0f3fa
racheltsai2020 <rachel.tsai@sjsu.edu>
2024-03-28 01:43:18 -0700

    update

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
 const axios =require('axios');
 
+const host = process.env.HOST || 'http://localhost:3000';
+
 describe('HTTP page status', ()=>{
     test('index.html load ok', async()=>{
-        const res = await axios.get('http://localhost:5500/index.html');
+        const res = await axios.get('${host}/index.html');
         expect(res.status).toBe(200);
     });
     test('upload.html load ok', async()=>{
-        const res = await axios.get('http://localhost:5500/upload.html');
+        const res = await axios.get('${host}/upload.html');
         expect(res.status).toBe(200);
     });
 });
