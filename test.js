ee834cce873d45c8543410c762c6d2573616d748
racheltsai2020 <rachel.tsai@sjsu.edu>
2024-03-28 01:33:00 -0700

    update config.yml

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ const axios =require('axios');
 
 describe('HTTP page status', ()=>{
     test('index.html load ok', async()=>{
-        const res = await axios.get('http://example.com/index.html');
+        const res = await axios.get('http://127.0.0.1:5500/index.html');
         expect(res.status).toBe(200);
     });
     test('upload.html load ok', async()=>{
-        const res = await axios.get('http://example.com/upload.html');
+        const res = await axios.get('http://127.0.0.1:5500/upload.html');
         expect(res.status).toBe(200);
     });
 });
